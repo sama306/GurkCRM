e75a2da5b75210b4a967d02d4129f8fd673ed7bd
sama306 <valensamacoits1@gmail.com>
2026-07-17 19:39:47 -0300

    chore: ignore backend/node_modules and dist

--- backend/dist/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const env_1 = require("./config/env");
-app_1.app.listen(env_1.env.PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${env_1.env.PORT}`);
-    console.log(`📋 Health check: http://localhost:${env_1.env.PORT}/api/v1/health`);
-});
-//# sourceMappingURL=server.js.map
